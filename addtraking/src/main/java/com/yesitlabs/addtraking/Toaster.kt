06155f9d63662b2f63fa5f8212213aafc2a2575f
yashasvi00 <yashasvi@yesitlabs.pro>
2023-12-13 16:17:01 +0530

    13-12-2023

---
+++ addtraking/src/main/java/com/yesitlabs/addtraking/Toaster.kt
@@ -0,0 +1,12 @@
+package com.yesitlabs.addtraking
+
+import android.content.Context
+import android.widget.Toast
+
+class Toaster {
+    companion object{
+        public fun showToas(context:Context,msg:String){
+            Toast.makeText(context,msg,Toast.LENGTH_SHORT).show()
+        }
+    }
+}
