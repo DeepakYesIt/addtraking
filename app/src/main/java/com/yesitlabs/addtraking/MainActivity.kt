bdd545f445e4ed08c39c523c16084af39e50a22f
yashasvi00 <yashasvi@yesitlabs.pro>
2024-01-03 15:05:44 +0530

    03-01-2023

--- app/src/main/java/com/yesitlabs/addtraking/MainActivity.kt
+++ app/src/main/java/com/yesitlabs/addtraking/MainActivity.kt
@@ -1,13 +1,31 @@
 package com.yesitlabs.addtraking
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.ads.identifier.AdvertisingIdClient
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Toaster.showToas(this,"sdk library mag )-")
+        var  tc= findViewById<TextView>(R.id.tv_session)
+
+
+        AddTraking.time=AddTraking.StartSession(this)
+
+        tc.setOnClickListener(View.OnClickListener {
+            AddTraking.sendData(this,"Male","AGN7iEKQvr6ho79L","2023-01-05","yesitlabs@gmail.com"/*,diff*/)
+        })
+
     }
+
 }
